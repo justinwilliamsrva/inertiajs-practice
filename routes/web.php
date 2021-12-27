061495f6df45c544836988b0a740d27a3151d94c
Justin Williams <justinwdev@gmail.com>
2021-12-27 15:05:46 -0500

    Created two new components, added vue and inertiajs

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Home');
+});
+Route::get('/user', function () {
+    return inertia('User');
+});
+Route::get('/settings', function () {
+    return inertia('Settings');
 });
